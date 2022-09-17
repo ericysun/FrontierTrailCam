ee2f78a380a6e789510ef9c6e76f799879d203ba
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-09-17 15:44:38 -0700

    Add files via upload

--- camera_ap_storage/camera_ap_storage.ino
+++ camera_ap_storage/camera_ap_storage.ino
@@ -31,7 +31,7 @@
 int pictureNumber = 0;
 int sleep_time = TIME_TO_SLEEP  ;
 
-const char* ssid = "ESP32-CAM Access Point";
+const char* ssid = "Frontier TrailCam Access Point";
 const char* password = "123456789";
 RTC_DS3231 rtc;
 Preferences preferences ;
