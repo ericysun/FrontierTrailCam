cefa33402fe1353df767f2e1a15f7b94847c026b
Eric Sun <85909186+ericysun@users.noreply.github.com>
2025-05-06 11:09:26 -0700

    Update acknowledgements.c

--- camera_ap_storage/acknowledgements.c
+++ camera_ap_storage/acknowledgements.c
@@ -1,9 +1,13 @@
 /*********
+  For the camera webserver streamingcode:
   Rui Santos
   Complete instructions at https://RandomNerdTutorials.com/esp32-cam-projects-ebook/
-  
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-  For the camera code
+  For webserver UI styling:
+  W3 Schools CSS resources
+
+  PIN Layout and Hardware Info:
+  https://lastminuteengineers.com/getting-started-with-esp32-cam/
 *********/
